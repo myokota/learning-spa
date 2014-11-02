9b0f60cc4ab045684b381121565bab3bc463f603
Masashi YOKOTA <yktmss@gmail.com>
2014-11-02 18:29:34 -0500

    initial commit

---
+++ js/spa.js
@@ -0,0 +1,20 @@
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : true,
+  white  : true
+*/
+/*global $, spa:ture */
+
+var spa = (function () {
+	var initModule = function ($container) {
+		$container.html(
+			'<h1 style="display:inline-block; margin:25px;">'
+			+ 'Hello world!'
+			+ '</h1>'
+			);
+	};
+
+	return { initModule : initModule }
+}());
+
