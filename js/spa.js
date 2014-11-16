ebb8be222c2fabcd40bab5f8393466e65a5fceaa
Masashi YOKOTA <yktmss@gmail.com>
2014-11-17 08:08:01 +0900

    add click event handler to open chat-slider

--- js/spa.js
+++ js/spa.js
@@ -8,11 +8,15 @@
 
 var spa = (function () {
 	var initModule = function ($container) {
+	/*
 		$container.html(
 			'<h1 style="display:inline-block; margin:25px;">'
 			+ 'Hello world!'
 			+ '</h1>'
 			);
+	*/
+		console.log('spa: ' + $container.toString() );
+		spa.shell.initModule( $container );
 	};
 
 	return { initModule : initModule }
---
+++ js/spa.shell.js
@@ -0,0 +1,111 @@
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+/*global $, spa:ture */
+
+spa.shell = (function () {
+		var
+			configMap = {
+				main_html : toString()
+					+ '<div class="spa-shell-head">'
+						+ '<div class="spa-shell-head-logo"></div>'
+						+ '<div class="spa-shell-head-acct"></div>'
+						+ '<div class="spa-shell-head-search"></div>'
+					+ '</div>'
+					+ '<div class="spa-shell-main">'
+						+ '<div class="spa-shell-main-nav"></div>'
+						+ '<div class="spa-shell-main-content"></div>'
+					+ '</div>'
+					+ '<div class="spa-shell-foot"></div>'
+					+ '<div class="spa-shell-chat"></div>'
+					+ '<div class="spa-shell-modal"></div>',
+
+				chat_extend_time : 1000,
+				chat_retract_time: 300,
+				chat_extend_height: 450, 
+				chat_retract_height: 15,
+				chat_extended_title: 'Click to retract',
+				chat_retracted_title: 'Click to extend'
+
+
+			},
+			stateMap = { 
+				$container : null,
+		   		is_chat_retrected: true
+			},
+			jqueryMap = {},
+
+			setJqueryMap, toggleChat, onClickChat, initModule;
+
+		setJqueryMap = function () {
+			var $container = stateMap.$container;
+			jqueryMap = { 
+				$container : $container,
+		   		$chat : $container.find( '.spa-shell-chat' )
+			};
+		};
+
+		toggleChat = function( do_extend, callback ) {
+			var
+				px_chat_ht = jqueryMap.$chat.height(),
+				is_open = px_chat_ht === configMap.chat_extend_height,
+				is_closed = px_chat_ht === configMap.chat_retract_height,
+				is_sliding = ! is_open && ! is_closed;
+
+			if ( is_sliding ) { return false; }
+
+			if ( do_extend ) {
+				jqueryMap.$chat.animate(
+					{ height : configMap.chat_extend_height },
+					configMap.chat_extend_time,
+					function () {
+						jqueryMap.$chat.attr(
+							'title', configMap.chat_extended_title
+						);
+						stateMap.is_chat_retrected = false;
+						if ( callback ) { callback( jqueryMap.$chat ); }
+					}
+				);
+				return true;
+			}
+
+			jqueryMap.$chat.animate(
+				{ height : configMap.chat_retract_height },
+				configMap.chat_retract_time,
+				function () {
+						jqueryMap.$chat.attr(
+							'title', configMap.chat_extended_title
+						);
+						stateMap.is_chat_retrected = true;
+					if ( callback ) { callback( jqueryMap.$chat ); }
+				}
+			);
+			return true;
+
+		};
+
+		onClickChat = function ( event ) {
+			toggleChat( stateMap.is_chat_retrected );
+			return false;
+		};
+
+		initModule = function ( $container ) {
+			console.log('initModule: ' + $container.toString() );
+			stateMap.$container = $container;
+			$container.html( configMap.main_html );
+			setJqueryMap();
+
+			stateMap.is_chat_retrected = true;
+			jqueryMap.$chat
+				.attr( 'title', configMap.chat_retracted_title )
+				.click( onClickChat );
+		};
+
+		return {initModule : initModule };
+}());
+
+
+
