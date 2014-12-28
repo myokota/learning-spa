efeded7ac05ba63fc1aabf0cee815eb8c7d3d6f2
Masashi YOKOTA <yktmss@gmail.com>
2014-12-28 17:47:03 +0900

    add routes.js divided from app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,7 @@
 var connectHello, server,
     http = require( 'http' ),
     express = require( 'express' ),
+    routes = require( './routes' ),
     app = express(),
     server = http.createServer( app );
 
@@ -23,40 +24,7 @@ app.configure( 'production', function() {
   app.use( express.errorHandler() );
 });
 
-app.get( '/', function ( request, response ) {
-  response.redirect( '/spa.html' );
-});
-
-app.get( '/:obj_type/*?', function ( request, response, next ) {
-  response.contentType( 'json' );
-  next();
-});
-
-app.get( '/:obj_type/list', function ( request, response ) {
-  response.send({ title: request.params.obj_type + ' list' });
-});
-
-app.post( '/:obj_type/create', function ( request, response ) {
-  response.send({ title: request.params.obj_type + ' created' });
-});
-
-app.get( '/:obj_type/read/:id([0-9]+)', function ( request, response ) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-  });
-});
-
-app.post( '/:obj_type/update/:id([0-9]+)', function ( request, response ) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
-  });
-});
-
-app.get( '/:obj_type/delete/:id([0-9]+)', function ( request, response ) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-  });
-});
+routes.configRoutes( app, server );
 
 server.listen( 3000 );
 
