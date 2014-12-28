b7b3dc7f8f531a4f49ea02ab6028f332c5654bf7
Masashi YOKOTA <yktmss@gmail.com>
2014-12-28 16:56:43 +0900

    add app server on node

---
+++ webapp/app.js
@@ -0,0 +1,65 @@
+var connectHello, server,
+    http = require( 'http' ),
+    express = require( 'express' ),
+    app = express(),
+    server = http.createServer( app );
+
+app.configure( function () {
+  app.use( express.bodyParser() );
+  app.use( express.methodOverride() );
+  app.use( express.static( __dirname + '/public' ) );
+  app.use( app.router );
+});
+
+app.configure( 'development', function() {
+  app.use( express.logger() );
+  app.use( express.errorHandler({
+    dumpExceptions : true,
+    showStack : true
+  }) );
+});
+
+app.configure( 'production', function() {
+  app.use( express.errorHandler() );
+});
+
+app.get( '/', function ( request, response ) {
+  response.redirect( '/spa.html' );
+});
+
+app.get( '/:obj_type/*?', function ( request, response, next ) {
+  response.contentType( 'json' );
+  next();
+});
+
+app.get( '/:obj_type/list', function ( request, response ) {
+  response.send({ title: request.params.obj_type + ' list' });
+});
+
+app.post( '/:obj_type/create', function ( request, response ) {
+  response.send({ title: request.params.obj_type + ' created' });
+});
+
+app.get( '/:obj_type/read/:id([0-9]+)', function ( request, response ) {
+  response.send({
+    title: request.params.obj_type + ' with id ' + request.params.id + ' found'
+  });
+});
+
+app.post( '/:obj_type/update/:id([0-9]+)', function ( request, response ) {
+  response.send({
+    title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
+  });
+});
+
+app.get( '/:obj_type/delete/:id([0-9]+)', function ( request, response ) {
+  response.send({
+    title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
+  });
+});
+
+server.listen( 3000 );
+
+console.log( 'Express server listening on port %d in %s mode', 
+  server.address().port, app.settings.env );
+
