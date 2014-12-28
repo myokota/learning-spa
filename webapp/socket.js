c830414b4ceddddcd9f4372bc201c60c59e5c43b
Masashi YOKOTA <yktmss@gmail.com>
2014-12-28 19:10:18 +0900

    add to handle js using fs node module

--- webapp/socket.js
+++ webapp/socket.js
@@ -1,21 +1,43 @@
 var 
-  countUp,
+  setWatch,
+
   http = require( 'http' ),
   express = require( 'express' ),
   socketIo = require( 'socket.io' ),
+  fsHandle = require( 'fs' );
+
   app = express(),
   server = http.createServer( app ),
   io = socketIo.listen( server ),
-  countIdx = 0
-  ;
+  watchMap = {};
 
-countUp = function () {
-  countIdx++;
-  console.log( countIdx );
-  io.sockets.send( countIdx );
+setWatch = function ( url_path, file_type ) {
+  console.log( 'setWatch called on ' + url_path );
+
+  if ( ! watchMap[ url_path ] ) {
+    console.log( 'setting watch on ' + url_path );
+
+    fsHandle.watchFile(url_path.slice(1), function ( current, previous ) {
+      console.log( 'file accessed' );
+      if ( current.mtime !== previous.mtime ) {
+        console.log( 'file changed' );
+        io.sockets.emit( file_type, url_path );
+      }
+    });
+  watchMap[ url_path ] = true;
+  }
 };
-
+      
 app.configure( function() {
+  app.use( function ( request, response, next ) {
+    if ( request.url.indexOf( '/js/' ) >= 0 ) {
+      setWatch( request.url, 'script' );
+    }
+    else if ( request.url.indexOf( '/css/' ) >= 0 ) {
+      setWatch( request.url, 'stylesheet' );
+    }
+    next();
+  });
   app.use( express.static( __dirname + '/' ) );
 });
 
@@ -29,7 +51,3 @@ console.log( 'Express server listening on port %d in %s mode',
   app.settings.env
 );
   
-setInterval( countUp, 1000 );
-
-
-
