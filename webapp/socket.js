b678f74b4675cef58a0ffb6549741c21ffec7ac8
Masashi YOKOTA <yktmss@gmail.com>
2014-12-28 18:41:43 +0900

    add socket.io with test code

---
+++ webapp/socket.js
@@ -0,0 +1,35 @@
+var 
+  countUp,
+  http = require( 'http' ),
+  express = require( 'express' ),
+  socketIo = require( 'socket.io' ),
+  app = express(),
+  server = http.createServer( app ),
+  io = socketIo.listen( server ),
+  countIdx = 0
+  ;
+
+countUp = function () {
+  countIdx++;
+  console.log( countIdx );
+  io.sockets.send( countIdx );
+};
+
+app.configure( function() {
+  app.use( express.static( __dirname + '/' ) );
+});
+
+app.get( '/', function ( request, response ) {
+  response.redirect( '/socket.html' );
+});
+
+server.listen( 3000 );
+console.log( 'Express server listening on port %d in %s mode',
+  server.address().port, 
+  app.settings.env
+);
+  
+setInterval( countUp, 1000 );
+
+
+
